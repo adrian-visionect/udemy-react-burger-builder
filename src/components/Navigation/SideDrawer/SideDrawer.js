063272e006d4dc83a3567460066e13a1c0252145
adtokarz <63449220+adtokarz@users.noreply.github.com>
2020-09-17 18:05:57 +0200

    update

---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Logo from '../../Logo/Logo';
+import styles from './SideDrawer.module.css';
+import NavigationItems from '../NavigationItems/NavigationItems';
+const sideDrawer = (props) => {
+  return (
+    <div className={styles.SideDrawer}>
+      <Logo />
+      <nav>
+        <NavigationItems />
+      </nav>
+    </div>
+  );
+};
+
+export default sideDrawer;
